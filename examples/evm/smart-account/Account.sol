b5a3a0c5ee73791d77460d9a6fac28b818bfb5e5
Miftahul Arifin <miftahul97@gmail.com>
2024-09-17 16:16:15 +0700

    fix: rename

--- examples/evm/smart-account/Account.sol
+++ examples/evm/smart-account/Account.sol
@@ -1,18 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Account {
+import "./EntryPoint.sol";
+import "./SignatureVerifier.sol";
+
+contract Account is SignatureVerifier {
     address public owner;
+    address private immutable _signer;
+    EntryPoint private immutable _entryPoint;
+
+    uint8 private constant SIGNATURE_V = 27;
 
     event OwnerChanged(address indexed oldOwner, address indexed newOwner);
+    event TransactionExecuted(address indexed dest, uint256 value, bytes data);
 
-    constructor(address _owner) {
+    constructor(
+        address _owner,
+        address _entryPointAddr,
+        bytes32 _messageHash,
+        bytes32 _r,
+        bytes32 _s
+    ) {
         owner = _owner;
+        _entryPoint = EntryPoint(_entryPointAddr);
+        _signer = recoverSigner(_messageHash, _r, _s, SIGNATURE_V);
     }
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner can call this function");
+        _onlyOwner();
         _;
+    }
+
+    function _onlyOwner() internal view {
+        //directly from EOA owner, or through the account itself (which gets redirected through execute())
+        require(msg.sender == owner || msg.sender == address(this), "only owner");
+    }
+
+    function getSigner() public view returns (address) {
+        return _signer;
     }
 
     // Allow the owner to change the account owner
@@ -21,44 +46,32 @@ contract Account {
         owner = newOwner;
     }
 
-    // Validate user operation by checking the signature
-    function validateUserOp(bytes32 userOpHash, bytes memory signature) public view returns (bool) {
-        // Recover the signer address from the hash and signature
-        address signer = recoverSigner(userOpHash, signature);
-        return signer == owner;
+    // Validate operation by checking the signature
+    function validateOperation(bytes32 messageHash, bytes32 r, bytes32 s) external view returns (bool) {
+        return verifySignature(messageHash, r, s, SIGNATURE_V, _signer);
     }
 
-    // ECDSA Signature recovery function
-    function recoverSigner(bytes32 hash, bytes memory signature) internal pure returns (address) {
-        require(signature.length == 65, "Invalid signature length");
-
-        bytes32 r;
-        bytes32 s;
-        uint8 v;
-
-        // Extract the signature parameters
-        assembly {
-            r := mload(add(signature, 32))
-            s := mload(add(signature, 64))
-            v := byte(0, mload(add(signature, 96)))
-        }
-
-        // Version of signature should be 27 or 28
-        if (v < 27) {
-            v += 27;
-        }
-
-        require(v == 27 || v == 28, "Invalid signature version");
-
-        // Recover the signer
-        return ecrecover(hash, v, r, s);
+    // Allow the contract to execute arbitrary transactions
+    function executeTransaction(address dest, uint256 value, bytes calldata data) external returns (bool) {
+        _requireFromEntryPointOrOwner();
+        bool success = _call(dest, value, data);
+        emit TransactionExecuted(dest, value, data);
+        return success;
     }
 
-    // Allow owner to execute arbitrary transactions
-    function executeTransaction(address target, uint256 value, bytes memory data) public onlyOwner returns (bytes memory) {
-        (bool success, bytes memory returnData) = target.call{value: value}(data);
-        require(success, "Transaction failed");
-        return returnData;
+    // Require the function call went through EntryPoint or owner
+    function _requireFromEntryPointOrOwner() internal view {
+        require(msg.sender == address(_entryPoint) || msg.sender == owner, "account: not Owner or EntryPoint");
+    }
+
+    function _call(address target, uint256 value, bytes memory data) internal returns (bool) {
+        (bool success, bytes memory result) = target.call{value: value}(data);
+        if (!success) {
+            assembly {
+                revert(add(result, 32), mload(result))
+            }
+        }
+        return success;
     }
 
     // Allow the contract to receive Ether
--- test/smart-account/entryPoint.test.js
+++ test/smart-account/entryPoint.test.js
@@ -1,8 +1,6 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-const { sha256, defaultAbiCoder, toUtf8Bytes, solidityPack } = require('ethers/lib/utils');
-const { ecsign } = require('ethereumjs-util');
-const { Account } = require("@multiversx/sdk-core/out");
+const { sha256 } = require('ethers/lib/utils');
 
 const TX_BYTES_A_1 = "CoECCv4BCiUvaW50ZXJjaGFpbmF1dGguaWNhdXRoLnYxLk1zZ1N1Ym1pdFR4EtQBCi1jb3Ntb3MxenlwcWE3NmplN3B4c2R3a2ZhaDZtdTlhNTgzc2p1NnhxdDNtdjYSCWNoYW5uZWwtMBqQAQocL2Nvc21vcy5iYW5rLnYxYmV0YTEuTXNnU2VuZBJwCi1jb3Ntb3MxenlwcWE3NmplN3B4c2R3a2ZhaDZtdTlhNTgzc2p1NnhxdDNtdjYSLWNvc21vczFtZ3A3cW52dW1obGNxNmU0ejJxMDVyMjlkY2hjeXVnMHozenhlZxoQCgV1YmV0YRIHMjAwMDAwMCCAoPHCsTQSWApQCkYKHy9jb3Ntb3MuY3J5cHRvLnNlY3AyNTZrMS5QdWJLZXkSIwohApC+f+iGx0i+gOmLNA0UGNC/54ZWde5ZfZ2FBSZSAIXwEgQKAggBGAESBBDAmgwaB2FscGhhLTE="
 const TX_BYTES_A_2 = "CoECCv4BCiUvaW50ZXJjaGFpbmF1dGguaWNhdXRoLnYxLk1zZ1N1Ym1pdFR4EtQBCi1jb3Ntb3MxenlwcWE3NmplN3B4c2R3a2ZhaDZtdTlhNTgzc2p1NnhxdDNtdjYSCWNoYW5uZWwtMBqQAQocL2Nvc21vcy5iYW5rLnYxYmV0YTEuTXNnU2VuZBJwCi1jb3Ntb3MxenlwcWE3NmplN3B4c2R3a2ZhaDZtdTlhNTgzc2p1NnhxdDNtdjYSLWNvc21vczFtZ3A3cW52dW1obGNxNmU0ejJxMDVyMjlkY2hjeXVnMHozenhlZxoQCgV1YmV0YRIHMjAwMDAwMCCAoPHCsTQSWApQCkYKHy9jb3Ntb3MuY3J5cHRvLnNlY3AyNTZrMS5QdWJLZXkSIwohApC+f+iGx0i+gOmLNA0UGNC/54ZWde5ZfZ2FBSZSAIXwEgQKAggBGAISBBDAmgwaB2FscGhhLTE="
@@ -10,39 +8,8 @@ const SIGNATURE_A_1 = "GHUe6rGUxcUuzLnjYJ8qE+qYoHkTjcKdr2c5Ea4mCJlzrMvDi6sZZCNO4
 const SIGNATURE_A_2 = "0A5QeQgokPkkOxxYgdw+shCF+3nwr1eq4fxhax+kQ+J3TycL0JpeF4CuSDyvnUHwGMQk4ecx/15cl9CzJtUbgw=="
 
 const EXPECTED_SIGNER = "0x07557D755E777B85d878D34861cd52126524a155"
-
-describe("EntryPoint and Account Contracts", function () {
-  let entryPoint;
-  let account;
-  let owner;
-  let user;
-
-  beforeEach(async function () {
-    // Get accounts
-    [owner, user] = await ethers.getSigners();
-
-    // Deploy EntryPoint contract
-    const EntryPoint = await ethers.getContractFactory("EntryPoint");
-    entryPoint = await EntryPoint.deploy();
-    await entryPoint.deployed();
-
-    // Call EntryPoint to create a new account
-    const newAccountTx = await entryPoint.createAccount(user.address);
-    const txReceipt = await newAccountTx.wait();
-
-    // Get the new account address from emitted events
-    const newAccountAddress = txReceipt.events[0].args.accountAddress;
-    
-    // Check if the new account was deployed correctly
-    account = await ethers.getContractAt("Account", newAccountAddress);
-  });
-
-  it("should create a new account and assign the correct owner", async function () {
-    expect(await account.owner()).to.equal(user.address);
-  });
-});
   
-describe("EntryPointWithoutSignature and Account Contracts", function () {
+describe("EntryPoint and Account Contracts", function () {
   let entryPoint;
   let account;
   let owner;
@@ -53,9 +20,9 @@ describe("EntryPointWithoutSignature and Account Contracts", function () {
     const MockGateway = await ethers.getContractFactory("MockGateway");
     const mockGateway = await MockGateway.deploy();
 
-    // Deploy EntryPointWithoutSignature contract
-    const EntryPointWithoutSignature = await ethers.getContractFactory("EntryPointWithoutSignature");
-    entryPoint = await EntryPointWithoutSignature.deploy(mockGateway.address, "0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6");
+    // Deploy EntryPoint contract
+    const EntryPoint = await ethers.getContractFactory("EntryPoint");
+    entryPoint = await EntryPoint.deploy(mockGateway.address, "0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6");
     await entryPoint.deployed();
 
     const commandId = ethers.utils.formatBytes32String("commandId");
@@ -79,7 +46,7 @@ describe("EntryPointWithoutSignature and Account Contracts", function () {
     const tx = await entryPoint.execute(commandId, sourceChain, sourceAddress, payload);
     const receipt = await tx.wait();
     const event = receipt.events.find((e) => e.event === "AccountCreated");
-    account = await ethers.getContractAt("AccountWithoutSignature", event.args[0]);
+    account = await ethers.getContractAt("Account", event.args[0]);
 
     await owner.sendTransaction({
       to: account.address,
@@ -117,7 +84,6 @@ describe("EntryPointWithoutSignature and Account Contracts", function () {
       ["uint8", "address", "bytes32", "bytes32", "bytes32", "address", "uint256", "bytes"],
       [2, account.address, messageHash, r, s, recipient, amountToSend, "0x"],
     );
-    console.log("payload :", payload)
     await entryPoint.execute(commandId, sourceChain, sourceAddress, payload);
 
     // Verify that the owner received the Ether
